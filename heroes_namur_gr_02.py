ce712498f4f71d9ad71e0afc98342a206ed6bce1
Martin Danhier <martin.danhier@student.unamur.be>
2019-02-22 10:14:35 +0100

    Decoupage

---
+++ heroes_namur_gr_02.py
@@ -0,0 +1,70 @@
+### UI ###
+# Display user interface
+
+def display_ui (players, map):
+    pass
+
+def get_coords_to_color(coords):
+    pass
+
+def create_stats(players):
+    pass
+
+### INPUT ###
+# Check input command written by player
+
+def create_character():
+    pass
+
+def parse_command (command):
+    pass
+
+### GENERATION ###
+# Use file to apply parameters of HoN
+
+def read_file(path):
+    pass
+
+### CLEANING ###
+# Clean entities & attributes bonus / malus
+
+def clean():
+    pass
+
+### SPECIAL ABILITY ###
+# Step : use abilities (!before CLEANING & MOVE AND FIGHT!)
+
+def special_abilities():
+    pass
+
+### MOVE AND FIGHT ###
+# Choice : Move or attack ?
+
+def attack():
+    pass
+
+def moving_on():
+    pass
+
+
+### CREATURES ###
+
+def process_creatures():
+    pass
+
+### AI ###
+
+def think(players, map):
+
+### TOOLS ###
+
+def is_coord_in_range(coords_1, coords_2):
+    pass
+
+### MAIN ###
+#!One function can be lauched for 1 party!
+
+def main():
+    pass
+
+main()
