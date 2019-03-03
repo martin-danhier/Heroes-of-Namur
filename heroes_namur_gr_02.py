c7c94fa6485500292d8a1f908019ff0ca14eb5cb
Jonathan Nhouyvanisvong <jonathan.nhouyvanisvong@student.unamur.be>
2019-03-03 02:47:28 +0100

    read_file specification done

--- heroes_namur_gr_02.py
+++ heroes_namur_gr_02.py
@@ -3,7 +3,7 @@
 * database : name_class {lvl : default_stats_table}
 * map : size - spawns {players - bot - citadel}
 
-**don't forget nb turns of game in dictionary ===> map['turn']**
+**don't forget nb turns of game in dictionary**
 """
 
 ### UI ###
@@ -11,42 +11,38 @@
 
 
 def display_ui(players, map):
-    """ Displays the board (with colors) and statistics of the players.
+    """ Displays the board (with colors) and statistics of the players, based on two dictionaries: players and map
 
-    Parameters
-    ----------
-    players : data of player heroes and creatures (dict)
-    map: data of the map (spawns, spur, size, etc...) (dict)
+    Parameters:
+    -----------
+    players : information about players and their characters : heroes and creatures (dict)
+    map : information about the map, with spur and player spawns coordinates (dict)
 
     Notes
     -----
-    For the formats of players and map, see rapport_gr_02_part_02.
+    For the format of players and map, see rapport_gr_02_part_02.
 
-    Version
-    -------
-    specification : Guillaume Nizet (v.2 02/03/19)
+    Version:
+    --------
+    specification : Guillaume Nizet (v.1 01/03/19)
     """
     pass
 
 
 def get_coords_to_color(coords):
-    """ Generates the coordinates that need to be colored around the given coordinates. 
+    """ Returns a list of coordinates that need to be colored, which are around the given coordinates 'coords'
 
-    Parameters
-    ----------
+    Parameters:
+    -----------
     coords : coordinates which need to have their surrounding coordinates colored (tuple)
 
-    Returns
-    -------
-    coords_to_color : coordinates that need to be colored (tuple).
-
-    Notes
-    -----
-    A typical 'coord' tuple is in the format ( row (int), column (int) ).
+    Returns:
+    --------
+    coords_to_color : coordinates that need to be colored (list)
 
     Version:
     --------
-    specification : Guillaume Nizet (v.2 02/03/19)
+    specification : Guillaume Nizet (v.1 1/03/19)
     """
     pass
 
@@ -56,11 +52,11 @@ def create_stats(players):
 
     Parameters:
     -----------
-    players : data of player heroes and creatures (dict)
+    players : information about players and their characters : heroes and creatures (dict)
 
     Returns
     -------
-    stats : a multiline string countaining the graphical representation of the stats. (str)
+    stats : a multiline string countaining the UI of the stats. (str)
 
     Notes
     -----
@@ -68,29 +64,25 @@ def create_stats(players):
 
     Version
     -------
-    specification : Martin Danhier (v.3 02/03/19)
+    specification : Martin Danhier (v.1 29/02/19)
     """
     pass
 
 
 def convert_to_true_coords(coords):
-    """ Converts the given tile coordinates to absolute char coordinates.
+    """ Converts a pair of coords to absolute char coords.
 
     Parameters
     ----------
-    coords: the tile coordinates to convert (tuple).
-    
+    coords: a tuple of format (row (int), col (int))
+
     Returns
     -------
-    true_coords: the coordinates of the center character of the tile. (tuple)
-
-    Notes
-    -----
-    A typical 'coord' tuple is in the format ( row (int), column (int) ).
+    true_coords: a tuple of format (row (int), col (int)) countaining the coords of the center char of the tile
 
     Version
     -------
-    specification : Martin Danhier (v.2 02/03/19)
+    specification : Martin Danhier (v.1 01/02/19)
 
     """
     pass
@@ -123,65 +115,44 @@ def create_line_char(first, cross, last, y, x, color, width):
 # Check input command written by player
 
 
-def create_character(players, map, command, player):
-    """ Parse the input command and create the players.
+def create_character(players, database):
+    """ Attribute the character chosen by player and saved in main dictionary.
 
     Parameters
     ----------
-    players : data of player heroes and creatures. (dict)
-    map: data of the map (spawns, spur, size, etc...). (dict)
-    command: the input string to be parsed. (str)
-    player: the player executing the command. (str)
+    players: player data that will contain the chosen heroes (dictionary)
+    database: containing default stats characters (dictionary)
 
     Notes
     -----
-    For the formats of 'players' and 'map', see rapport_gr_02_part_02.
-    The 'players' dictionary is updated: one or more heroes can be added.
+    For the format of players and database, see rapport_gr_02_part_02.
+    The players dictionary is updated: a new hero is added.
 
     Version
     -------
-    specification : Jonathan Nhouyvanisvong (v.3 02/03/2019)
+    specification : Jonathan Nhouyvanisvong (v.2 01/03/19)
 
     """
     pass
 
 
 def parse_command(command):
-    """ Parse the input into a list of actions.
-    
-    Parameters
-    ----------
-    command: the input string to be parsed (str)
-    
-    Returns
-    -------
-    actions: a list of actions (list of dict)
-
-    Notes
-    -----
-    The format of the "command" string is destribed in the instructions, p14.
-    A typical "action" dict looks like this : 
-        {
-            'hero' : hero_name (str),
-            'player' : player_name (str),
-            'action' : action (str) (can be the name of an ability (as 'fulgura') or 'attack' or 'fight'),
-            'target' : ( x (int), y (int) ) >> optional
-        }
-    
-    Version
-    -------
-    specification : Martin Danhier (v.1 02/03/2019)"""
+    """"""
 
 
 ### GENERATION ###
 # Use file to apply parameters of HoN
 
-def read_file(path):
+def read_file(path, dict_param):
     """Read parameter file to complete structure of the game
-    ...
+    Parameters
+    ----------
+    path: the path of the folder in which are the parameter file (str)
+    dict_param: will contain the data about differents entities for the game (dictionary)
+
     Version
     -------
-    specification : Jonathan Nhouyvanisvong (v.1 22/02/19)
+    specification : Jonathan Nhouyvanisvong (v.2 03/03/19)
     """
     pass
 
@@ -190,21 +161,26 @@ def read_file(path):
 
 
 def clean(players):
-    """ Cleans the board (managing death and levels).
+    """ Cleans the board by:
+        - Removing the creatures that were killed
+        - Putting back on their spawn platforms heroes that were killed
+        - Giving victory points to players that are in the killed creatures areas
+        - Leveling up heroes if they have enough victory points
 
-    Parameters
-    ----------
-    players : data of player heroes and creatures. (dict)
+    Parameters:
+    -----------
+    players : information about players and their characters : heroes and creatures (dict)
 
-    Notes
-    -----
-    'players' can be updated if a hero or a creature is killed.
-    For the format of 'players', see rapport_gr_02_part_02.
-    More details about the rules of the cleaning can be found in the instructions, p10.
+    Notes:
+    ------
+    'players' can be modified:
+        - Heroes coordinates can be updated if they are killed 
+        - Creatures can be removed from the dictionary if they are killed
+        - Victory points and level of heroes can be updated if a creature is killed
 
-    Version
-    -------
-    specification : Guillaume Nizet (v.4 02/03/19)    
+    Version:
+    --------
+    specification : Guillaume Nizet (v.1 01/03/19)    
     """
     pass
 
@@ -213,9 +189,10 @@ def clean(players):
 
 
 def special_abilities():
-    """ Prepare special abilities to use
+    """Prepare special abilities to use
     Parameters
     ----------
+    
 
     Returns
     -------
@@ -234,57 +211,36 @@ def special_abilities():
 ### MOVE AND FIGHT ###
 # Choice : Move or attack ?
 
-def attack(order, players, map):
-    """ Tries to execute the given attack order.
-
+def attack(heroes_players):
+    """Prepare and store the attack chosen by player
     Parameters
     ----------
-    order: the attack order. (dict)
-    players : data of player heroes and creatures. (dict)
-    map: data of the map (spawns, spur, size, etc...). (dict)
+    hero_player: containing data for the hero who will attack and the enemy who will be attacked (dictionary)
 
     Notes
-    -----
-    The 'order' dictionary is in the following format: 
-        {
-            'hero' : hero_name (str),
-            'player' : player_name (str),
-            'action' : 'fight',
-            'target' : ( x (int), y (int) )
-        }
-    For the formats of 'players' and 'map', see rapport_gr_02_part_02.
-    The 'players' dictionary may be updated.
+    -------
+    parse_command() store in a list the move made. And the statistic of heroes_players updates automatically.
 
     Version
     -------
-    specification : Jonathan Nhouyvanisvong (v.3 03/03/19)
+    specification : Jonathan Nhouyvanisvong (v.2 22/02/19)
     """
     pass
 
-def moving_on(order, players, map):
-    """ Tries to execute the given move order.
-
+def moving_on(hero_player, map):
+    """Prepare and update the movement chosen by player
     Parameters
     ----------
-    order: the attack order. (dict)
-    players : data of player heroes and creatures. (dict)
-    map: data of the map (spawns, spur, size, etc...). (dict)
+    hero_player: hero who wants to move (dictionary)
+    map: map coordinates to check differents entities (dictionary)
 
     Notes
-    -----
-    The 'order' dictionary is in the following format: 
-        {
-            'hero' : hero_name (str),
-            'player' : player_name (str),
-            'action' : 'move',
-            'target' : ( x (int), y (int) )
-        }
-    For the formats of 'players' and 'map', see rapport_gr_02_part_02.
-    The 'players' dictionary may be updated.
+    -------
+    parse_command() store in a list the move made. And the coordinates of hero_player updates automatically.
 
     Version
     -------
-    specification : Jonathan Nhouyvanisvong (v.3 03/03/19)
+    specification : Jonathan Nhouyvanisvong (v.2 01/03/19)
 
     """
     pass
@@ -293,49 +249,32 @@ def moving_on(order, players, map):
 ### CREATURES ###
 
 def process_creatures(players):
-    """ Automatically computes an action for a creature to perform.
+    """ Automatically computes an action for a creature to perform:
+            - does nothing if there is no hero in its area of influence or if it's not affected by any special ability
+            - goes towards the nearest player if there is at least one hero in its area of influence or if it's affected by a special ability
+            - attacks a player if it is right next to it
 
     Parameters:
     -----------
-    players : data of player heroes and creatures (dict)
+    players : information about players and their characters : heroes and creatures (dict)
 
     Notes:
     ------
-    For the format of 'players', see rapport_gr_02_part_02.
-    The 'players' dict can be modified.
+    'players' can be modified:
+        - Creatures coordinates can be updated if they move towards a player
+        - Players health can be updated if they get attacked by a creature
 
     Version:
     --------
-    specification : Guillaume Nizet (v.2 03/03/19)
+    specification : Guillaume Nizet (v.1 01/03/19)
     """
     pass
 
-
 ### AI ###
 
-def think (players, map, database, player):
-    """ Entry point of the IA: process data and create an "order string".
-    
-    Parameters
-    ----------
-    players : data of player heroes and creatures (dict)
-    map: data of the map (spawns, spur, size, etc...) (dict)
-    database : data of hero classes (dict)
-    player : the name of the AI player (str)
 
-    Returns
-    -------
-    command : string countaining the orders to execute (str)
-
-    Notes
-    -----
-    For the formats of 'players', 'map' and 'database', see rapport_gr_02_part_02.
-    The format of 'command' is described in the instructions, p14.
-
-    Version
-    -------
-    specificaation : Martin Danhier (v.1 02/03/19)
-    """
+def think(players, map):
+    pass
 
 ### TOOLS ###
 
@@ -348,11 +287,11 @@ def is_coord_in_range(coords_1, coords_2):
 
 
 def main():
-    """ Manages the global course of the in-game events.
+    """ Main loop for the game, calls every function during each iteration
     
     Version:
     --------
-    specification : Guillaume Nizet (v.2 03/03/19)
+    specification : Guillaume Nizet (v.1 01/03/19)
     """
     pass
 
